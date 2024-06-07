cf1b4ef3a11d160370b806b93f5b68579634f0fe
Joseph <joeyparlamenti@yahoo.com>
2024-06-06 23:43:55 -0400

    dynamic card creation

--- src/components/Hand/CardData.tsx
+++ src/components/Hand/CardData.tsx
@@ -1,16 +1,45 @@
 import { Card } from "./CardHand";
 
-function setCardData(): Card[]{
-    let cardData = []
-    for (var i = 1; i > 14; i++) {
-        let iteratedCard1 = new Card({
-            id: i,
-            CardValue: i + 1,
-            CardName: `${i + 1}`;
-            CardColor: true,
-            CardSuit: "Spade"
-        })
-        cardData.push(iteratedCard1)
+function setDeck(): Card[]{
+    let deckData = []
+    let cardColorValue = false
+
+    for (var i = 1; i > 9; i++) {
+        let iteratedSuitCard = function(cardSuitValue: string): Card { 
+            switch (cardSuitValue) {
+                case 'Heart' || 'Diamond':
+                    cardColorValue = false 
+                    break;
+                case 'Spade' || 'Club':
+                    cardColorValue = true 
+                    break;
+                default:
+                    throw new Error('Invalid Value');                 
+            }
+            
+            let topDeckCard = new Card({
+                id: i,
+                CardValue: i + 1,
+                CardName: `${i + 1}`,
+                CardColor: cardColorValue,
+                CardSuit: cardSuitValue
+            })
+            let bottomDeckCard = new Card({
+                id: i + 54,
+                CardValue: i + 1,
+                CardName: `${i + 1}`,
+                CardColor: cardColorValue,
+                CardSuit: cardSuitValue
+            })
+        } 
+
+        iteratedSuitCard("Heart")
+        iteratedSuitCard("Spade")
+        iteratedSuitCard("Diamond")
+        iteratedSuitCard("Club")
+
+
+        deckData.push(iteratedCard1)
     }
-    return cardData
+    return deckData
 }
--- src/components/Hand/CardHand.tsx
+++ src/components/Hand/CardHand.tsx
@@ -1,36 +1,36 @@
 export class Card {
 
-    id: number;
-    CardValue: number;
-    CardName: string;
-    CardColor: boolean;
-    CardSuit: string;
+  id: number;
+  CardValue: number;
+  CardName: string;
+  CardColor: boolean;
+  CardSuit: string;
 
-    constructor(props:Card) {
-        const {id, CardValue, CardName, CardColor, CardSuit} = props
-        
-        this.id = id;
-        this.CardValue = CardValue;
-        this.CardName = CardName;
-        this.CardColor = CardColor;
-        this.CardSuit = CardSuit;
-      }
+  constructor(props:Card) {
+      const {id, CardValue, CardName, CardColor, CardSuit} = props
+      
+      this.id = id;
+      this.CardValue = CardValue;
+      this.CardName = CardName;
+      this.CardColor = CardColor;
+      this.CardSuit = CardSuit;
+    }
 }
 
 export class Hand {
-    id: number;
-    Cards?: Card[];
-    Meld?: boolean;
-    hasDiscarded?: boolean;
-    hadDrawn?: boolean;
+  id: number;
+  Cards?: Card[];
+  Meld?: boolean;
+  hasDiscarded?: boolean;
+  hadDrawn?: boolean;
 
-    constructor(props:Hand) {
-        const {id, Cards, Meld, hasDiscarded, hadDrawn} = props
-        
-        this.id = id;
-        this.Cards = Cards;
-        this.Meld = Meld;
-        this.hasDiscarded = hasDiscarded;
-        this.hadDrawn = hadDrawn;
-      }
+  constructor(props:Hand) {
+      const {id, Cards, Meld, hasDiscarded, hadDrawn} = props
+      
+      this.id = id;
+      this.Cards = Cards;
+      this.Meld = Meld;
+      this.hasDiscarded = hasDiscarded;
+      this.hadDrawn = hadDrawn;
+    }
 }
